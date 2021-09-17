a7f1332fd22bba599a350d01d31081a0c8dc56ac
Bold1234 <86370177+Bold1234@users.noreply.github.com>
2021-09-17 11:37:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:8000,
+        loop:true
+    });
+
+    
